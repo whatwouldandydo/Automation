e7a603f9ab718a3b5824d47f5cf2d24337a47cc2
whatwouldandydo <andysodo@hotmail.com>
2020-12-07 22:22:01 -0800

    Added project outline and import modules

---
+++ DevOps/7_bot_guessing_game.py
@@ -0,0 +1,32 @@
+"""
+Date: 2020-12-05
+This Python script will do the following:
+(1) Use Generator to store large data of random numbers
+(2) Compare each random number from the Generator with the Correct number
+(3) If the comparison is a match, log the number, date/time, and how long was the guess
+(4) Then autosave the log file to Github
+(5) Autoload the Python script when the system started with crontab
+"""
+
+### Import modules
+import pexpect
+import logging
+import time
+import datetime
+import concurrent.futures
+import subprocess
+
+
+### Create custom logging
+
+
+### Create random integer with generator
+
+
+### Compare random integer with corrected number
+
+
+### Run comparision as multithread
+
+
+### 
