445e1233498b1b38e32997a38190caea16920a9c
Andy Bosco <andysodo@hotmail.com>
2020-12-14 19:49:58 -0800

    Working code and clean up

--- DevOps/7_bot_guessing_game.py
+++ DevOps/7_bot_guessing_game.py
@@ -17,11 +17,6 @@ import concurrent.futures
 import subprocess
 import random
 
-timer1 = time.time()
-t2 = time.ctime()
-# print(timer1, t2)
-
-
 ### Create custom logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -33,51 +28,31 @@ logger.addHandler(file_handler)
 ### Create random integer with generator
 def generate_numbers(gnumber):
     for number in range(gnumber):
-        # number = random.random()
-        # number = str(number).split(".")[1]
-        # number = str(number).split("e")[0]
-        # number = int(str(number).split(".")[1])
-        number = random.randint(1,10)
+        number = random.random()
+        number = str(number).split(".")[1]
+        number = str(number).split("e")[0]
         number = int(number)
         yield number
 
 num_of_guesses = int(time.time_ns())
-# print(num_of_guesses)
         
-random_numbers = generate_numbers(10) #Use num_of_guesses for argument
-# print(next(random_numbers))
-# print(num_of_guesses)
+random_numbers = generate_numbers(1000) #Use num_of_guesses for argument
 
 ### Compare random integer with corrected number
 def compare_numbers(guess):
-    # correct_number = random.random()
-    # correct_number = str(correct_number).split(".")[1]
-    # correct_number = str(correct_number).split("e")[0]
-    # correct_number = int(correct_number)
-    correct_number = random.randint(1,10)
-    # print(f"correct_number {correct_number}")
+    correct_number = random.random()
+    correct_number = str(correct_number).split(".")[1]
+    correct_number = str(correct_number).split("e")[0]
+    correct_number = int(correct_number)
 
-    # for guess in cnumber:
-        # print(guess)
     if guess == correct_number:
         print(f"The correct number {guess} takes {time.time() - timer1} seconds to find.")
         logger.debug(f"The correct number {guess} takes {time.time() - timer1} seconds to find.")
-        # print(f"guess, correct_number {guess} {correct_number}")
         cd_cmd = subprocess.run("cd /home/do/Projects/Automation/Logs/", shell=True)
-        print(cd_cmd)
         git_checkout_cmd = subprocess.run("git checkout master", shell=True)
         git_add_cmd = subprocess.run("git add 7_Bot_Guessing_Game.log", shell=True)
         git_commit_cmd = subprocess.run(f"git commit -m 'Commit {guess} on {time.ctime()}'", shell=True)
         git_push_cmd = subprocess.run("git push origin master", shell=True)
-    # else:
-        # print(f"WRONG {guess} It takes {time.time() - timer1}")
-    #     cd_cmd = subprocess.run("cd ~/Projects/Automation/Logs", shell=True)
-    #     cd_cmd = subprocess.run("ls", shell=True)
-
-    # print("NO CORRECT GUESS")
-
-# compare = compare_numbers(random_numbers)
-
 
 ### Run comparision as multithread
 with concurrent.futures.ThreadPoolExecutor() as executor:
